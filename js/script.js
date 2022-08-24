7ae1391240c22f638abfaf4bca4b5aedd728e6e8
spangen87 <rikard.spangmyr@bestlas.se>
2022-08-24 19:13:17 +0000

    Try new function

--- js/script.js
+++ js/script.js
@@ -10,9 +10,25 @@
 //     $(this).removeClass('makeBlue').addClass('makeRed');
 // });
 
+// $('.document').ready(function() {
+//     $(".box").on("click", function() {
+//         var classNames = $(this).attr("class").split(" ");
+//         $("." + classNames[1]).css("background-color", "red");
+//     })
+// });
+
 $('.document').ready(function() {
     $(".box").on("click", function() {
-        var classNames = $(this).attr("class").split(" ");
-        $("." + classNames[1]).css("background-color", "red");
-    })
-})
+        var classNames = $(this).attr(class).split(" ");
+        var boxClass = classNames[0];
+        var className = classNames[1];
+        if ($(this).css("background.color") == "rgb(255, 0, 0)") {
+            // If this object is already red, turn it black
+            $("." + className).css("background-color", "#000");
+        } else {
+            // Else turn all elememts with a box class black and then change the color of all element with the same class name as the clicked element to red.
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
+        });
+    })        
